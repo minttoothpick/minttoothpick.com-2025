e12aa48f94290e54a2854aacdbd1c9fc4d25296d
Jeremy Jones <jeremyjones@Jeremys-MacBook-Pro.local>
2025-02-28 11:37:38 -0500

    Getting set up

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 module.exports = (config) => {
+
   return {
+    // Parse .html with Nunjucks
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist',
     },
   };
+
 };
---
+++ src/index.md
@@ -0,0 +1,5 @@
+---
+title: 'minttoothpick'
+layout: 'layouts/home.html'
+---
+Wowie!
